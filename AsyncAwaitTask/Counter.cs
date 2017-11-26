214f5e9d854561f5082d4b19cf5dcaae32669732
nikita <mishinnikitam@gmail.com>
2017-11-26 23:06:46 +0300

    Initial commit

---
+++ AsyncAwaitTask/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace AsyncAwaitTask
+{
+    public class Counter
+    {
+        private int _index = 0;
+        public int GetCounter() => _index;
+        public int Increment() => Interlocked.Increment(ref _index);
+        public int Decrement() => Interlocked.Decrement(ref _index);
+
+        public void Reset()
+        {
+            _index = 0;
+        }
+    }
+}
---
+++ AsyncAwaitTask/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace AsyncAwaitTask
+{
+    public class Loader
+    {
+        private Url _url;
+        private readonly int _depth;
+        private Counter _counter;
+
+        public Loader(Url url, int depth = 1)
+        {
+            _url = new Url(url.Value);
+            _depth = 1;
+        }
+
+        private async void G(Url url, int currentDepth)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
