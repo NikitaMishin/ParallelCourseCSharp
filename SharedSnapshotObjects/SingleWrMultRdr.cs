c058615ea587fe7e6c78037fdd84f9184cd86326
nikita <mishinnikitam@gmail.com>
2017-11-28 16:36:23 +0300

    comleted func.Needed some tests

--- SharedSnapshotObjects/SingleWrMultRdr.cs
+++ SharedSnapshotObjects/SingleWrMultRdr.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Xml.Schema;
+﻿using System.Linq;
 
 
 namespace AtomicSnapshot
@@ -9,10 +6,11 @@ namespace AtomicSnapshot
     // bounded version
     public class SingleWrMultRdr
     {
-        private object _writerLock = new object();
+        // private object _writerLock = new object();
         public readonly int Readers;
-        private Register[] _registers;
-        private bool[,] _q;
+
+        private readonly Register[] _registers;
+        private readonly bool[,] _q;
 
         public SingleWrMultRdr(int readers = 4)
         {
@@ -34,15 +32,33 @@ namespace AtomicSnapshot
         public int[] ScanI(int registerIndex)
         {
             var i = registerIndex;
-            var moved = Enumerable.Repeat(default(bool), Readers);
+            var moved = Enumerable.Repeat(default(bool), Readers).ToArray();
             while (true)
             {
-                for (int j = 0; j < Readers; j++)
+                for (var j = 0; j < Readers; j++) //HandShake
                     _q[i, j] = _registers[i].P[j]; //sure??
                 var a = Collect();
                 var b = Collect();
-                //TODO 
-                throw new NotImplementedException();
+                var status = true;
+                for (var k = 0; k < Readers; k++)
+                {
+                    if (a[k].P[i] == b[k].P[i] && a[k].P[i] == _q[i, k] && a[k].Toogle == b[k].Toogle) continue;
+                    status = false;
+                    break;
+                }
+                if (status)
+                {
+                    return b.Select((register, i1) => register.Value).ToArray();
+                }
+                for (var j = 0; j < Readers; j++)
+                {
+                    if (a[j].P[i] == _q[i, j] && b[j].P[i] == _q[i, j] && a[j].Toogle == b[j].Toogle) continue;
+                    if (moved[j])
+                    {
+                        return b[j].SnapShot;
+                    }
+                    moved[j] = !moved[j];
+                }
             }
         }
 
@@ -55,6 +71,7 @@ namespace AtomicSnapshot
                 f[j] = !_q[j, i];
             }
             var snapShot = ScanI(i);
+            // is there need CMPEXGN?
             _registers[i].Value = value;
             _registers[i].Toogle = !_registers[i].Toogle;
             _registers[i].P = f;
@@ -64,7 +81,7 @@ namespace AtomicSnapshot
         private Register[] Collect() //aka return copy
         {
             var registersCopy = new Register[Readers];
-            for (int i = 0; i < Readers; i++)
+            for (var i = 0; i < Readers; i++)
             {
                 registersCopy[i] = _registers[i].Copy();
             }
